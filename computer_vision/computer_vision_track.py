7a7b83282d9ff11257b0b500be7b78492f1508d7
Cyber9unk <92269361+cyber9unk@users.noreply.github.com>
2022-02-15 04:07:38 +0300

    grayscale_conversion
    Grayscale conversion added. The reason for that is it makes the algorithm run essentially three times as fast because instead of 3 colors (RGB), it's just running black and white.

--- computer_vision/computer_vision_track.py
+++ computer_vision/computer_vision_track.py
@@ -2,7 +2,6 @@ import cv2
 
 # Image 1
 img_raw = 'car1.jpg'
-
 img_raw = "images/img_cars/cars_back/car1.jpg"
 
 # pre-trained car classifier
@@ -11,10 +10,20 @@ trained_classifier = 'car_detector.xml'
 # create an opencv image
 img = cv2.imread(img_raw)
 
-# Display the image with cars spotted.
-cv2.imshow('Bumbleous Image Detector', img) 
+# convert to grayscale (needed for haar cascade algorithm)
+gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Don't autoclose (Wait and listen for a key press)
+
+# Create a car classifier
+car_tracker = cv2.CascadeClassifier(trained_classifier)
+
+
+# Display image with car's spotted.
+cv2.imshow('Bumbleous Image Detector', gray_scale)
+
+ 
+ # Don't autoclose (Wait and listen for a key press)
 cv2.waitKey()
  
-print("Operation Completed")  
+ 
+print("Operation Completed") 
